c9beaf643ec84bcbe3257afbf3e3431a6c23c977
Ghifari-s <ghifaripooh@gmail.com>
2024-11-13 08:22:11 +0700

    add order-gateway.js

--- actions/orders-action.js
+++ actions/orders-action.js
@@ -1,50 +1,51 @@
+const { addOrderGateway, getAllOrderGateway } = require("../gateways/orders-gateway");
+
 // add order
-module.exports.addOrderAction =  (req, res) => {
-    const order = req.body;
-    order.order_id = orderid++;
-    orders.push(order);
-    res.status(201).json({
-      message: "order berhasil ditambah",
-      order_id: order.order_id,
-    });
-}
+module.exports.addOrderAction = (req, res) => {
+  const order = req.body;
+  addOrderGateway(order);
+  res.status(201).json({
+    message: "order berhasil ditambah",
+    order_id: order.order_id,
+  });
+};
 
 // get all order
 module.exports.getAllOrderAction = (req, res) => {
-    res.status(200).json(orders);
-}
+  res.status(200).json(getAllOrderGateway());
+};
 
 // get order by id
 module.exports.getOrderByIdAction = (req, res) => {
-    const id = parseInt(req.params.id);
-    const order = orders.find((p) => p.order_id === id)
-    if (order){
-        res.status(200).json(order)
-    } else{
-        res.status(404).json({message: "order tdk ditemukan"})
-    }
-}
+  const id = parseInt(req.params.id);
+  
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404).json({ message: "order tdk ditemukan" });
+  }
+};
 
 // update order by id
 module.exports.updateOrderByIdAction = (req, res) => {
-    const id = parseInt(req.params.id); 
-    const orderIndex = orders.findIndex((p) => p.order_id === id);
-    if (orderIndex !== -1) {
-      orders[orderIndex] = { ...orders[orderIndex], ...req.body }; 
-      res.status(200).json({ message: "order berhasil diperbarui" }); 
-    } else {
-      res.status(404).json({ message: "order tidak ditemukan" }); 
-    }
-}
+  const id = parseInt(req.params.id);
+  const result = updateOrderByIdGateway(id,req.body)
+  if (result) {
+    res.status(200).json({ message: "order berhasil diperbarui" });
+  } else {
+    res.status(404).json({ message: "order tidak ditemukan" });
+  }
+};
 
 // delete order by id
 module.exports.deleteOrderByIdAction = (req, res) => {
-    const id = parseInt(req.params.id); 
-    const orderIndex = orders.findIndex((p) => p.order_id === id); 
-    if (orderIndex !== -1) {
-      orders.splice(orderIndex, 1); 
-      res.status(200).json({ message: "order berhasil dihapus" }); 
-    } else {
-      res.status(404).json({ message: "order tidak ditemukan" }); 
-    }
+  const id = parseInt(req.params.id);
+  const orderIndex = orders.findIndex((p) => p.order_id === id);
+  if (orderIndex !== -1) {
+    orders.splice(orderIndex, 1);
+    res.status(200).json({ message: "order berhasil dihapus" });
+  } else {
+    res.status(404).json({ message: "order tidak ditemukan" });
   }
+};
+
--- gateways/orders-gateway.js
+++ gateways/orders-gateway.js
@@ -1,2 +1,30 @@
 let orders = [];
 let orderid = 1;
+
+// add order
+module.exports.addOrderGateway = (order) => {
+  order.order_id = orderid++;
+  orders.push(order);
+};
+
+// get all order
+module.exports.getAllOrderGateway = () => {
+  return orders;
+};
+
+// get order by id
+module.exports.getOrderByIdGateway = (id) => {
+  const order = orders.find((p) => p.order_id === id);
+  return order;
+};
+
+// update order by id
+module.exports.updateOrderByIdGateway = (id,data) => {
+    const orderIndex = orders.findIndex((p) => p.order_id === id);
+    if (orderIndex !== -1) {
+        orders[orderIndex] = { ...orders[orderIndex], ...data };
+        return true
+      } else {
+        return true
+      }
+}
